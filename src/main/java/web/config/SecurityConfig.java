7fd92f697f934afdad781be15f0e5b73755e00d6
prod.god <prod.god666@gmail.com>
2021-09-29 11:42:30 +0300

    x

--- src/main/java/web/config/SecurityConfig.java
+++ src/main/java/web/config/SecurityConfig.java
@@ -1,23 +1,36 @@
 package web.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import web.config.handler.LoginSuccessHandler;
+import web.service.UserDetailsServiceImpl;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final UserDetailsServiceImpl userDetailsService;
+    private final LoginSuccessHandler loginSuccessHandler;
+
+    @Autowired
+    public SecurityConfig(@Qualifier("userDetailsServiceImpl") UserDetailsServiceImpl userDetailsService, LoginSuccessHandler loginSuccessHandler, UserDetailsServiceImpl userDetailsServiceImpl) {
+        this.userDetailsService = userDetailsService;
+        this.loginSuccessHandler = loginSuccessHandler;
+    }
+
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("ADMIN").password("ADMIN").roles("ADMIN");
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 
     @Override
@@ -26,12 +39,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 // указываем страницу с формой логина
                 .loginPage("/login")
                 //указываем логику обработки при логине
-                .successHandler(new LoginSuccessHandler())
+                .successHandler(loginSuccessHandler)
                 // указываем action с формы логина
                 .loginProcessingUrl("/login")
                 // Указываем параметры логина и пароля с формы логина
-                .usernameParameter("j_username")
-                .passwordParameter("j_password")
+                .usernameParameter("login")
+                .passwordParameter("password")
+//                .defaultSuccessUrl("/hello", true)
                 // даем доступ к форме логина всем
                 .permitAll();
 
@@ -49,13 +63,30 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 // делаем страницу регистрации недоступной для авторизированных пользователей
                 .authorizeRequests()
                 //страницы аутентификаци доступна всем
-                .antMatchers("/login").anonymous()
+                .antMatchers("/login", "/registration").anonymous()
                 // защищенные URL
-                .antMatchers("/hello").access("hasAnyRole('ADMIN')").anyRequest().authenticated();
+                .antMatchers("/admin/*").access("hasRole('ROLE_ADMIN')")
+                .antMatchers("/user/*").access("hasAnyRole('ROLE_ADMIN', 'ROLE_USER')")
+
+                .anyRequest().authenticated();
+    }
+
+    // Необходимо для шифрования паролей
+    @Bean
+    public static BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        return authenticationProvider;
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -1,30 +1,103 @@
 package web.controller;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+import web.model.Role;
+import web.model.User;
+import web.service.interfaces.RoleService;
+import web.service.interfaces.UserService;
 
-import java.util.ArrayList;
-import java.util.List;
+
+import java.security.Principal;
+import java.util.HashSet;
+import java.util.Set;
 
 @Controller
-@RequestMapping("/")
 public class UserController {
 
-	@RequestMapping(value = "hello", method = RequestMethod.GET)
-	public String printWelcome(ModelMap model) {
-		List<String> messages = new ArrayList<>();
-		messages.add("Hello!");
-		messages.add("I'm Spring MVC-SECURITY application");
-		messages.add("5.2.0 version by sep'19 ");
-		model.addAttribute("messages", messages);
-		return "hello";
-	}
+    private final UserService userService;
+    private final RoleService roleService;
+    String message = "Register new user";
 
-    @RequestMapping(value = "login", method = RequestMethod.GET)
+    @Autowired
+    public UserController(UserService userService, RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
+    @GetMapping("/hello")
+    public String getUsername(Principal principal, ModelMap model) {
+        String name;
+        try {
+            name = principal.getName();
+        } catch (NullPointerException e) {
+            name = "";
+        }
+        model.addAttribute("current_user", "Hello, " + name);
+        return "hello";
+    }
+
+    @GetMapping("login")
     public String loginPage() {
+
+
+        if (roleService.findAll().isEmpty()) {
+            roleService.add(new Role("ROLE_USER"));
+            roleService.add(new Role("ROLE_ADMIN"));
+        }
+        if (userService.findByLogin("admin") == null) {
+            Set<Role> adminRoles = new HashSet<>();
+            adminRoles.add(roleService.getByName("ROLE_ADMIN"));
+            adminRoles.add(roleService.getByName("ROLE_USER"));
+            User admin = new User("admin", "123", "admin", "admin", 30, "admin@mail.com", adminRoles);
+            userService.update(admin);
+        }
         return "login";
     }
+
+    @GetMapping("user")
+    public String userPage(Principal principal, ModelMap modelMap) {
+        modelMap.addAttribute("current_user", userService.findByLogin(principal.getName()));
+        return "user";
+    }
+
+    @PostMapping("/user/deleteAcc")
+    public String deleteUser(Principal principal) {
+        userService.deleteById(userService.findByLogin(principal.getName()).getId());
+        return "/login";
+    }
+
+    @GetMapping("/registration")
+    public String registration(ModelMap model) {
+
+        model.addAttribute("userForm", new User());
+        model.addAttribute("message", message);
+        return "registration";
+    }
+
+    @PostMapping("/registration")
+    public String addUser(@ModelAttribute("userForm") User userForm, ModelMap model) {
+        Set<Role> roles = new HashSet<>();
+        roles.add(roleService.getByName("ROLE_USER"));
+
+        if (!userForm.getPassword().equals(userForm.getPasswordConfirm())) {
+            message = "Incorrect password input";
+            return "redirect:/registration";
+        }
+
+        userForm.setRoles(roles);
+        try {
+            userService.update(userForm);
+        } catch (Exception e) {
+            message = "User with that login already exists";
+            return "redirect:/registration";
+        }
+
+        return "redirect:/login";
+    }
+
 
 }
